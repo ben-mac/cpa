dd436d87f698748c326399d79b715be4d27b2cc1
Ben Macdonell <benjamin.macdonell@gmail.com>
2018-05-15 17:26:20 -0700

    adding job postings

---
+++ templates/job-postings.php
@@ -0,0 +1,72 @@
+<?php
+
+/* Template Name: Job Postings Page */
+
+get_header();
+
+
+?>
+
+<div id="main-content">
+
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-md-9">
+
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+					<h1 class="entry-title main_title"><?php the_title(); ?></h1>
+
+					<div class="entry-content">
+					<?php
+            the_content();
+          ?>
+					</div> <!-- .entry-content -->
+
+
+				<?php 
+				  $temp = $wp_query; 
+				  $wp_query = null; 
+				  $wp_query = new WP_Query(); 
+				  $wp_query->query('showposts=5&post_type=job_posting'.'&paged='.$paged); 
+
+				  while ($wp_query->have_posts()) : $wp_query->the_post(); 
+				?>
+
+						<div class="item-job-posting">
+							<a href="<?php echo get_permalink(); ?>"><h3 class="title"><?php the_field('job_title'); ?></h3></a>
+							<p><strong>Employer: </strong> <?php the_field('organization'); ?></p>
+							<p><strong>Location: </strong> <?php the_field('location'); ?></p>
+							<p><strong>Closing Date: </strong><?php the_field('job_closing_date'); ?></p>
+							<p> <a target="_blank" href="<?php the_field('job_description'); ?>" >Job Description</a></p>
+							<p><a target="_blank" href="<?php the_field('job_url'); ?>">Website</a></p>
+						</div>		  
+
+				<?php endwhile; ?>
+
+				<nav>
+				    <?php previous_posts_link('&laquo; Newer Job Postings') ?>
+				    <?php next_posts_link('Older Job Postings &raquo;') ?>
+				</nav>
+
+				<?php 
+				  $wp_query = null; 
+				  $wp_query = $temp;  // Reset
+				?>
+				</article> <!-- .et_pb_post -->
+
+			<?php endwhile; ?>
+
+			</div> <!-- left content-->
+      <div class="col-sm-12 col-md-3">
+			<?php get_sidebar(); ?>
+      </div>
+		</div> <!-- row -->
+	</div> <!-- .container -->
+
+
+</div> <!-- #main-content -->
+
+<?php get_footer(); ?>
